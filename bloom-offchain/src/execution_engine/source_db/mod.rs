d9e37b371ffd1b36d9dc8b26f414b236fbe4b5cc
oskin1 <ilya.arcadich@gmail.com>
2023-11-30 14:31:03 +0300

    InMemorySourceDB impl.

--- bloom-offchain/src/execution_engine/source_db/mod.rs
+++ bloom-offchain/src/execution_engine/source_db/mod.rs
@@ -1,7 +1,30 @@
+use std::collections::HashMap;
+
 use crate::execution_engine::SourceId;
 
 pub trait SourceDB<Src> {
     /// Query resolved source by [SourceId].
     /// Source is always available.
-    fn get(&mut self, source_id: SourceId) -> Src;
+    fn take_unsafe(&mut self, source_id: SourceId) -> Src;
+    fn put(&mut self, source_id: SourceId, source: Src);
+    fn remove(&mut self, source_id: SourceId);
+}
+
+pub struct InMemorySourceDB<Src> {
+    store: HashMap<SourceId, Src>
+}
+
+impl<Src> SourceDB<Src> for InMemorySourceDB<Src> {
+    fn take_unsafe(&mut self, source_id: SourceId) -> Src {
+        self.store.remove(&source_id)
+            .expect("Source is supposed to always be available")
+    }
+
+    fn put(&mut self, source_id: SourceId, source: Src) {
+        self.store.insert(source_id, source);
+    }
+
+    fn remove(&mut self, source_id: SourceId) {
+        self.store.remove(&source_id);
+    }
 }
