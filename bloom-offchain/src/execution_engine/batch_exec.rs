974b067dee2c5d05a48bf940af73c6b65275f666
oskin1 <ilya.arcadich@gmail.com>
2023-12-20 13:25:14 +0300

    Docs.

--- bloom-offchain/src/execution_engine/batch_exec.rs
+++ bloom-offchain/src/execution_engine/batch_exec.rs
@@ -1,6 +1,6 @@
 /// Defines how to execute a particular on-chain action with some entity.
 /// Effect of execution are supposed to be accumulated in [BatchAcc] (usually a TX builder).
-/// Execution possibly results into a [Next] state of the entity.
+/// Execution results into a [Next] state of the entity.
 pub trait BatchExec<BatchAcc, Next, Ctx, Err> {
     fn try_exec(self, accumulator: BatchAcc, context: Ctx) -> Result<(BatchAcc, Next, Ctx), Err>;
 }
