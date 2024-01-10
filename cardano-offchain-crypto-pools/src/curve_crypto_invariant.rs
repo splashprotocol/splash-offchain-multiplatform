eff9f52229cb55f2d034c89de3d46c65629ae6de
AOranov <a.oranov@spectrumlabs.fi>
2024-01-10 18:17:03 +0300

    CurveCrypto invariant value calculation.

--- cardano-offchain-crypto-pools/src/curve_crypto_invariant.rs
+++ cardano-offchain-crypto-pools/src/curve_crypto_invariant.rs
@@ -129,14 +129,158 @@ pub fn check_crypto_invariant_extremum(
     err_left > err_eq && err_right > err_eq
 }
 
+/// CurveCrypto invariant value numerical calculation procedure.
+/// \
+/// Note: input balances must be reduced to a common denominator (precision).
+/// \
+/// Function solves numerically (relative to `d` while all other parameters are known) the following equation :
+/// \
+/// \
+/// `k * d^(n - 1) * Sum(reserves) + Prod(reserves) = k * d^n + (d / n)^n`,
+/// \
+/// \
+/// Coefficient `k` is:
+/// \
+/// \
+/// `k = a * k0 * gamma ^ 2 / (gamma + 1 - k0)^2`,
+/// \
+/// \
+/// where
+/// \
+/// \
+/// `k0 = Prod(reserves) * n^n / d^n`.
+/// \
+/// \
+/// Variables involved are:
+///
+/// * `a` - Amplification coefficient;
+/// * `d` - Invariant value;
+/// * `n` - Number of tradable assets;
+/// * `gamma` - Distance coefficient;
+/// * `reserves = vec![asset_{i}_balance; n]`.
+/// \
+/// \
+///
+/// # Arguments
+/// * `balances` -Reserves balances;
+/// * `n` - Number of tradable assets;
+/// * `a` - Amplification coefficient of the CurveCrypto invariant.
+///
+/// # Outputs
+/// * `d` - value of the CurveCrypto invariant.
+
+pub fn calculate_crypto_invariant(
+    balances: &Vec<U512>,
+    n_assets: &u32,
+    a: &U512,
+    gamma_num: &U512,
+    gamma_denom: &U512,
+) -> U512 {
+    assert_eq!(balances.len(), (*n_assets).try_into().unwrap());
+    // Constants commonly used in calculations:
+    let unit = U512::from(1);
+    let n = U512::from(*n_assets);
+    let nn = U512::from(n_assets.pow(*n_assets));
+
+    // Invariant calculation with the Newton-Raphson method:
+    let s = balances.iter().copied().reduce(|x, y| x + y).unwrap();
+    let p = balances.iter().copied().reduce(|x, y| x * y).unwrap();
+
+    let mut d = s.clone();
+    let mut abs_d_error = d;
+    while abs_d_error > unit {
+        let d_previous = d;
+        let dn = vec![d; usize::try_from(n).unwrap()]
+            .iter()
+            .copied()
+            .reduce(|x, y| x * y)
+            .unwrap();
+        // k0 = 1 for stablecoins
+        let k0 = p * nn / dn;
+
+        let k = *a * k0 * *gamma_num * *gamma_num
+            / ((*gamma_num + *gamma_denom - k0 * *gamma_denom)
+                * (*gamma_num + *gamma_denom - k0 * *gamma_denom));
+        let inv_left = k * dn / d * s + p;
+        let inv_right = k * dn + dn / nn;
+
+        let f_value = inv_left - inv_right;
+
+        let c = dn / (nn / n);
+        let g = p * p * (nn / dn) * (nn / dn);
+        let e = (*gamma_denom + *gamma_num - g * *gamma_denom) / *gamma_denom;
+        let f_derivative_value_denom = d * d;
+        let h = (unit + unit) * nn * nn * n * p;
+        let f_derivative_value_num = c * d
+            + *a * *gamma_num * *gamma_num * nn * p / (*gamma_denom * *gamma_denom * dn)
+                * ((h * s - h * d) * p / dn + s * e * dn)
+                * e
+                * e;
+        let f_derivative_value = f_derivative_value_num / f_derivative_value_denom;
+
+        d = d_previous + f_value / f_derivative_value;
+        abs_d_error = if d >= d_previous {
+            d - d_previous
+        } else {
+            d_previous - d
+        }
+    }
+
+    // Fine tuning of the calculated value:
+    let mut inv_err =
+        calculate_crypto_invariant_error_from_totals(n_assets, &nn, &p, &s, &a, &d, &gamma_num, &gamma_denom);
+    let inv_err_upper = calculate_crypto_invariant_error_from_totals(
+        n_assets,
+        &nn,
+        &p,
+        &s,
+        &a,
+        &(d + unit),
+        &gamma_num,
+        &gamma_denom,
+    );
+    let inv_err_lower = calculate_crypto_invariant_error_from_totals(
+        n_assets,
+        &nn,
+        &p,
+        &s,
+        &a,
+        &(d - unit),
+        &gamma_num,
+        &gamma_denom,
+    );
+
+    if !((inv_err < inv_err_upper) && (inv_err < inv_err_lower)) {
+        let mut inv_err_previous = inv_err + unit;
+
+        while inv_err < inv_err_previous {
+            inv_err_previous = inv_err;
+            d = d + unit;
+            inv_err = calculate_crypto_invariant_error_from_totals(
+                n_assets,
+                &nn,
+                &p,
+                &s,
+                &a,
+                &d,
+                &gamma_num,
+                &gamma_denom,
+            );
+        }
+
+        d = d - unit
+    }
+    d
+}
+
 mod test {
     use primitive_types::U512;
     use rand::seq::SliceRandom;
     use rand::Rng;
 
     use crate::curve_crypto_invariant::{
-        calculate_crypto_invariant_error_from_balances, calculate_crypto_invariant_error_from_totals,
-        check_crypto_invariant_extremum,
+        calculate_crypto_invariant, calculate_crypto_invariant_error_from_balances,
+        calculate_crypto_invariant_error_from_totals, check_crypto_invariant_extremum,
     };
 
     #[test]
@@ -210,6 +354,23 @@ mod test {
             let is_extremum =
                 check_crypto_invariant_extremum(&n, &a, &gamma_num, &gamma_denom, &balances_calc, &d);
             assert!(is_extremum)
+        }
+    }
+
+    #[test]
+    fn calculate_invariant_test() {
+        let n_assets_set: Vec<u32> = vec![2, 3, 4];
+        let mut rng = rand::thread_rng();
+        for _ in 0..1000 {
+            let a = U512::from(rng.gen_range(1..2000));
+            let n = *n_assets_set.choose(&mut rand::thread_rng()).unwrap();
+            let balance: u64 = rng.gen();
+            let balances_calc = vec![U512::from(balance); n as usize];
+            let gamma_num = U512::from(rng.gen_range(1..10_000));
+            let gamma_denom = U512::from(1_000_000_000u64);
+            let inv = calculate_crypto_invariant(&balances_calc, &n, &a, &gamma_num, &gamma_denom);
+
+            assert_eq!(inv, U512::from(n) * U512::from(balance))
         }
     }
 }
