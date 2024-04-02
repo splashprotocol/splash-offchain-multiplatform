71ff1701f440b37f0226448eb286c060d06f5470
oskin1 <ilya.arcadich@gmail.com>
2024-04-02 14:03:02 +0300

    Remove redundant code.

--- splash-testing-cardano/src/lucid.ts
+++ splash-testing-cardano/src/lucid.ts
@@ -1,7 +1,7 @@
 import { Blockfrost, Lucid } from 'https://deno.land/x/lucid@0.10.7/mod.ts';
 
 export async function getLucid() {
-  const token = "preprod5w7d2sFOTsIH8xxepGXxv2FZmqkGAqhi";
+  const token = await Deno.readTextFile('./token.txt');
   return Lucid.new(
     new Blockfrost('https://cardano-preprod.blockfrost.io/api/v0', token),
     'Preprod',
--- splash-testing-cardano/src/wallet.ts
+++ splash-testing-cardano/src/wallet.ts
@@ -1,7 +1,7 @@
 import { Lucid } from 'https://deno.land/x/lucid@0.10.7/mod.ts';
 
 export async function setupWallet(lucid: Lucid) {
-  const seed = "uncle inherit chest series fox entry vague basic slab grunt carbon collect foot half purse usual dwarf you fuel sunset pull swamp gain diet";
+  const seed = await Deno.readTextFile('./seed.txt');
   lucid.selectWalletFromSeed(seed);
 }
 
