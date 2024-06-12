d4ddd0a9dcc82a3d42bbbb4c152a2ecafad5e908
oskin1 <ilya.arcadich@gmail.com>
2024-06-12 11:42:41 +0300

    WIP.

--- bloom-offchain/src/execution_engine/liquidity_book/liquidity_bin.rs
+++
@@ -1,8 +0,0 @@
-use crate::execution_engine::liquidity_book::side::Side;
-use crate::execution_engine::liquidity_book::types::AbsolutePrice;
-
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
-pub struct Bin {
-    pub amount: Side<u64>,
-    pub price: AbsolutePrice,
-}
