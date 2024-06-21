98e5b11ff60d9a55f09136dc63721f696f97b98e
oskin1 <ilya.arcadich@gmail.com>
2024-06-21 11:47:27 +0300

    WIP.

--- bloom-offchain/src/execution_engine/liquidity_book/parallel.rs
+++ bloom-offchain/src/execution_engine/liquidity_book/parallel.rs
@@ -1,22 +1,18 @@
-use bounded_integer::BoundedU64;
+use std::cmp::min;
 use std::fmt::Debug;
-use std::mem;
-
-use log::trace;
 
 use algebra_core::monoid::Monoid;
 use spectrum_offchain::data::Stable;
 
-use crate::execution_engine::liquidity_book::core::{
-    MatchmakingAttempt, MatchmakingRecipe, MatchmakingStep, TakeInProgress,
-};
-use crate::execution_engine::liquidity_book::fragment::{Fragment, OrderState};
-use crate::execution_engine::liquidity_book::market_maker::MarketMaker;
+use crate::execution_engine::liquidity_book::core::{Make, MatchmakingAttempt, MatchmakingRecipe, MatchmakingStep, Take, TakeInProgress, TakerTrans, TryApply};
+use crate::execution_engine::liquidity_book::fragment::{Fragment, OrderState, TakerBehaviour};
+use crate::execution_engine::liquidity_book::market_maker::{MarketMaker, SpotPrice};
+use crate::execution_engine::liquidity_book::side::Side::{Ask, Bid};
 use crate::execution_engine::liquidity_book::side::{Side, SideM};
 use crate::execution_engine::liquidity_book::stashing_option::StashingOption;
-use crate::execution_engine::liquidity_book::state::TLBState;
-use crate::execution_engine::liquidity_book::types::AbsolutePrice;
-use crate::execution_engine::liquidity_book::{ExecutionCap, TLBFeedback};
+use crate::execution_engine::liquidity_book::state::{max_by_distance_to_spot, max_by_volume, TLBState};
+use crate::execution_engine::liquidity_book::types::{AbsolutePrice, InputAsset};
+use crate::execution_engine::liquidity_book::{linear_output_unsafe, ExecutionCap, TLBFeedback};
 
 /// TLB is a Universal Liquidity Aggregator (ULA), it is able to aggregate every piece of composable
 /// liquidity available in the market.
@@ -50,6 +46,15 @@ where
     }
 }
 
+impl<Taker, Maker, U> TLB<Taker, Maker, U>
+where
+    Maker: MarketMaker + Stable,
+{
+    fn spot_price(&self) -> Option<SpotPrice> {
+        None
+    }
+}
+
 impl<Taker, Maker, U> TemporalLiquidityBook<Taker, Maker> for TLB<Taker, Maker, U>
 where
     Taker: Stable + Fragment<U = U> + OrderState + Ord + Copy + Debug,
@@ -57,53 +62,105 @@ where
     U: Monoid + PartialOrd + Copy,
 {
     fn attempt(&mut self) -> Option<MatchmakingRecipe<Taker, Maker>> {
-        loop {
-            let mut batch: MatchmakingAttempt<_, _, U> = MatchmakingAttempt::empty();
-            let mut both_sides_tried = false;
-            while batch.execution_units_consumed() < self.execution_cap.soft {
-                if let Some(best_taker) = self.state.try_pick_fr(self.attempt_side, ok) {
-                    let take = TakeInProgress::new(best_taker);
-                    loop {
-                        if batch.execution_units_consumed() < self.execution_cap.soft {
-                            // 1. Take a chunk of remaining input from remainder
-                            // 2. Take liquidity from best counter-offer (takers/makers)
-                            let rem_side = take.target.side();
-                            let rem_price = rem_side.wrap(take.target.price());
-                            let maybe_price_counter_taker = self.state.best_fr_price(!rem_side);
-                            let chunk_offered = take.next_chunk_offered(self.step);
-                            let maybe_price_maker = self.state.preselect_market_maker(chunk_offered);
-                            match (maybe_price_counter_taker, maybe_price_maker) {
-                                (Some(price_counter_taker), maybe_price_maker)
-                                    if maybe_price_maker
-                                        .map(|(_, p)| price_counter_taker.better_than(p))
-                                        .unwrap_or(true) => {}
-                                (_, Some((maker_sid, price_maker))) if rem_price.overlaps(price_maker) => {}
-                                _ => {}
-                            }
+        let mut batch: MatchmakingAttempt<_, _, U> = MatchmakingAttempt::empty();
+        while batch.execution_units_consumed() < self.execution_cap.soft {
+            let spot_price = self.spot_price();
+            let strategy = |fs| {
+                spot_price
+                    .map(|sp| max_by_distance_to_spot(fs, sp))
+                    .unwrap_or(max_by_volume(fs))
+            };
+            if let Some(target_taker) = self.state.pick_taker(strategy) {
+                let take = TakeInProgress::new(target_taker);
+                let target_side = take.target.side();
+                let target_price = target_side.wrap(take.target.price());
+                let maybe_price_counter_taker = self.state.best_fr_price(!target_side);
+                let chunk_offered = take.next_chunk_offered(self.step);
+                let maybe_price_maker = self.state.preselect_market_maker(chunk_offered);
+                match (maybe_price_counter_taker, maybe_price_maker) {
+                    (Some(price_counter_taker), maybe_price_maker)
+                        if maybe_price_maker
+                            .map(|(_, p)| price_counter_taker.better_than(p))
+                            .unwrap_or(true) =>
+                    {
+                        if let Some(counter_taker) = self.state.try_pick_fr(!target_side, ok) {
+                            //fill target_taker <- counter_taker
                         }
-                        break;
                     }
-                }
-                self.attempt_side = !self.attempt_side;
-                break;
-            }
-            match MatchmakingRecipe::try_from(batch) {
-                Ok(ex_recipe) => return Some(ex_recipe),
-                Err(None) => {
-                    self.on_recipe_failed(StashingOption::Unstash);
-                    if mem::replace(&mut both_sides_tried, true) {
-                        trace!("Trying to matchmake on the other side: {}", self.attempt_side);
-                        continue;
+                    (_, Some((maker_sid, price_maker))) if target_price.overlaps(price_maker) => {
+                        if let Some(maker) = self.state.take_pool(&maker_sid) {
+                            //fill target_taker <- maker
+                        }
                     }
-                }
-                Err(Some(unsatisfied_fragments)) => {
-                    self.on_recipe_failed(StashingOption::Stash(unsatisfied_fragments));
-                    continue;
+                    _ => {}
                 }
             }
-            return None;
         }
+        None
+
+        // loop {
+        //     while batch.execution_units_consumed() < self.execution_cap.soft {
+        //         if let Some(best_taker) = self.state.try_pick_fr(self.attempt_side, ok) {
+        //             let take = TakeInProgress::new(best_taker);
+        //             loop {
+        //                 if batch.execution_units_consumed() < self.execution_cap.soft {
+        //                     // 1. Take a chunk of remaining input from remainder
+        //                     // 2. Take liquidity from best counter-offer (takers/makers)
+        //                     let rem_side = take.target.side();
+        //                     let rem_price = rem_side.wrap(take.target.price());
+        //                     let maybe_price_counter_taker = self.state.best_fr_price(!rem_side);
+        //                     let chunk_offered = take.next_chunk_offered(self.step);
+        //                     let maybe_price_maker = self.state.preselect_market_maker(chunk_offered);
+        //                     match (maybe_price_counter_taker, maybe_price_maker) {
+        //                         (Some(price_counter_taker), maybe_price_maker)
+        //                             if maybe_price_maker
+        //                                 .map(|(_, p)| price_counter_taker.better_than(p))
+        //                                 .unwrap_or(true) => {}
+        //                         (_, Some((maker_sid, price_maker))) if rem_price.overlaps(price_maker) => {}
+        //                         _ => {}
+        //                     }
+        //                 }
+        //                 break;
+        //             }
+        //         }
+        //         break;
+        //     }
+        // }
     }
+}
+
+fn fill_from_maker<Taker, Maker>(target_taker: Taker, maker: Maker) -> (TakerTrans<Taker>, TryApply<Make, Maker>) {
+    
+}
+
+fn fill_from_taker<Taker, F>(
+    target_taker: Taker,
+    counter_taker: Taker,
+    matchmaker: F,
+) -> (TakerTrans<Taker>, TakerTrans<Taker>)
+    where
+        Taker: Fragment + TakerBehaviour + Copy,
+        F: FnOnce(&Taker, &Taker) -> AbsolutePrice,
+{
+    let (ask, bid) = match target_taker.side() {
+        SideM::Bid => (counter_taker, target_taker),
+        SideM::Ask => (target_taker, counter_taker),
+    };
+    let price = matchmaker(&ask, &bid);
+    let quote_input = bid.input();
+    let demand_base = linear_output_unsafe(quote_input, Bid(price));
+    let supply_base = ask.input();
+    let (quote, base) = if supply_base > demand_base {
+        (quote_input, demand_base)
+    } else if supply_base < demand_base {
+        let quote_executed = linear_output_unsafe(supply_base, Ask(price));
+        (quote_executed, supply_base)
+    } else {
+        (quote_input, demand_base)
+    };
+    let next_bid = bid.with_applied_trade(quote, base);
+    let next_ask = ask.with_applied_trade(base, quote);
+    (next_bid, next_ask)
 }
 
 fn ok<'a, T>(_: &'a T) -> bool {
