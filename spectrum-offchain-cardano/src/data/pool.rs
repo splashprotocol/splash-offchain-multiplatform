2080496aae735cfd20f706b96e59168148b14f36
Alex Oranov <114924866+AOranov@users.noreply.github.com>
2024-08-09 20:23:10 +0300

    Available liquidity methods. (#125)
    * Available liquidity methods.
    
    * Abs price simplified.
    
    * Tests added, abs price output removed.
    
    ---------
    
    Co-authored-by: AOranov <a.oranov@spectrumlabs.fi>

--- spectrum-offchain-cardano/src/data/pool.rs
+++ spectrum-offchain-cardano/src/data/pool.rs
@@ -1,3 +1,5 @@
+use bloom_offchain::execution_engine::liquidity_book::market_maker::AvailableLiquidity;
+use num_rational::Ratio;
 use std::fmt::{Debug, Display, Formatter};
 
 use cml_chain::address::Address;
@@ -313,13 +315,20 @@ impl MarketMaker for AnyPool {
             StableCFMM(p) => p.liquidity(),
         }
     }
-
+    fn available_liquidity_on_side(&self, worst_price: OnSide<AbsolutePrice>) -> Option<AvailableLiquidity> {
+        match self {
+            PureCFMM(p) => p.available_liquidity_on_side(worst_price),
+            BalancedCFMM(p) => p.available_liquidity_on_side(worst_price),
+            StableCFMM(p) => p.available_liquidity_on_side(worst_price),
+        }
+    }
     fn is_active(&self) -> bool {
         match self {
             PureCFMM(p) => p.is_active(),
             BalancedCFMM(p) => p.is_active(),
             StableCFMM(p) => p.is_active(),
         }
+
     }
 }
 
