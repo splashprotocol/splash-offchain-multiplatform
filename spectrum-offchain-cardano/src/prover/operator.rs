eb1cc78ae44c64cc2d066d1c15931dea11962cca
oskin1 <ilya.arcadich@gmail.com>
2024-09-01 19:52:38 +0300

    WIP.

--- spectrum-offchain-cardano/src/prover/operator.rs
+++ spectrum-offchain-cardano/src/prover/operator.rs
@@ -1,10 +1,7 @@
 use cml_chain::builders::tx_builder::SignedTxBuilder;
-use cml_chain::builders::witness_builder::WitnessBuilderError;
 use cml_chain::crypto::utils::make_vkey_witness;
 use cml_chain::transaction::Transaction;
-use cml_core::serialization::Serialize;
 use cml_crypto::PrivateKey;
-use log::trace;
 use spectrum_cardano_lib::hash::hash_transaction_canonical;
 use spectrum_cardano_lib::transaction::OutboundTransaction;
 use spectrum_offchain::tx_prover::TxProver;
@@ -22,14 +19,6 @@ impl<'a> OperatorProver<'a> {
 impl<'a> TxProver<SignedTxBuilder, OutboundTransaction<Transaction>> for OperatorProver<'a> {
     fn prove(&self, mut candidate: SignedTxBuilder) -> OutboundTransaction<Transaction> {
         let body = candidate.body();
-        trace!(
-            "Transaction body CBOR to sign: {}",
-            hex::encode(body.to_cbor_bytes())
-        );
-        trace!(
-            "Transaction body hash to sign: {}",
-            hash_transaction_canonical(&body).to_hex()
-        );
         let signature = make_vkey_witness(&hash_transaction_canonical(&body), self.0);
         candidate.add_vkey(signature);
         match candidate.build_checked() {
