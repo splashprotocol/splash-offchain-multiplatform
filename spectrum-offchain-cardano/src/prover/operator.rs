c6eb89bcf9b9610c139cbb725ca31266431640e6
oskin1 <ilya.arcadich@gmail.com>
2024-09-01 19:38:03 +0300

    WIP.

--- spectrum-offchain-cardano/src/prover/operator.rs
+++ spectrum-offchain-cardano/src/prover/operator.rs
@@ -1,4 +1,5 @@
 use cml_chain::builders::tx_builder::SignedTxBuilder;
+use cml_chain::builders::witness_builder::WitnessBuilderError;
 use cml_chain::crypto::utils::make_vkey_witness;
 use cml_chain::transaction::Transaction;
 use cml_core::serialization::Serialize;
@@ -31,6 +32,9 @@ impl<'a> TxProver<SignedTxBuilder, OutboundTransaction<Transaction>> for Operato
         );
         let signature = make_vkey_witness(&hash_transaction_canonical(&body), self.0);
         candidate.add_vkey(signature);
-        candidate.build_unchecked().into()
+        match candidate.build_checked() {
+            Ok(tx) => tx.into(),
+            Err(err) => panic!("CML returned error: {}", err),
+        }
     }
 }
