d501d5c96b656043a80103df5057b771b59f8940
oskin1 <ilya.arcadich@gmail.com>
2024-09-02 11:40:27 +0300

    Debug.

--- spectrum-offchain-cardano/src/prover/operator.rs
+++ spectrum-offchain-cardano/src/prover/operator.rs
@@ -1,7 +1,9 @@
 use cml_chain::builders::tx_builder::SignedTxBuilder;
 use cml_chain::crypto::utils::make_vkey_witness;
 use cml_chain::transaction::Transaction;
+use cml_core::serialization::Serialize;
 use cml_crypto::PrivateKey;
+use log::trace;
 use spectrum_cardano_lib::hash::hash_transaction_canonical;
 use spectrum_cardano_lib::transaction::OutboundTransaction;
 use spectrum_offchain::tx_prover::TxProver;
@@ -18,12 +20,26 @@ impl<'a> OperatorProver<'a> {
 
 impl<'a> TxProver<SignedTxBuilder, OutboundTransaction<Transaction>> for OperatorProver<'a> {
     fn prove(&self, mut candidate: SignedTxBuilder) -> OutboundTransaction<Transaction> {
-        let body = candidate.clone().build_unchecked().body;
-        let signature = make_vkey_witness(&hash_transaction_canonical(&body), self.0);
+        let body = candidate.body();
+        let tx_hash = hash_transaction_canonical(&body);
+        trace!("Tx body hash to sign: {}", tx_hash.to_hex());
+        trace!("Tx body bytes to sign: {}", hex::encode(body.to_cbor_bytes()));
+        let signature = make_vkey_witness(&tx_hash, self.0);
         candidate.add_vkey(signature);
-        match candidate.build_checked() {
-            Ok(tx) => tx.into(),
+        trace!("Tx body hash after signing: {}", hash_transaction_canonical(&candidate.body()).to_hex());
+        trace!(
+            "Tx body bytes after signing: {}",
+            hex::encode(candidate.body().to_cbor_bytes())
+        );
+        let built_tx = match candidate.build_checked() {
+            Ok(tx) => tx,
             Err(err) => panic!("CML returned error: {}", err),
-        }
+        };
+        trace!("Tx body hash after building: {}", hash_transaction_canonical(&built_tx.body).to_hex());
+        trace!(
+            "Tx body bytes after building: {}",
+            hex::encode(built_tx.body.to_cbor_bytes())
+        );
+        built_tx.into()
     }
 }
