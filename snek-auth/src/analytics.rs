f8bcdb38d7c2b7190278a2469b9f2060d966b720
Bromel777 <alexbromel@gmail.com>
2024-11-08 23:45:16 +0300

    more debug

--- snek-auth/src/analytics.rs
+++ snek-auth/src/analytics.rs
@@ -2,7 +2,7 @@ use crate::analytics::LaunchType::{Common, Fair};
 use reqwest::Error;
 use spectrum_cardano_lib::Token;
 use std::time::Duration;
-use log::error;
+use log::{error, info};
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 #[serde(from = "String")]
@@ -65,13 +65,15 @@ impl Analytics {
             format!("{}.{}", token.0.to_hex(), hex::encode(token.1.as_bytes())),
         )];
 
+        info!("test: {}", format!("{}.{}", token.0.to_hex(), hex::encode(token.1.as_bytes())));
+        info!("self.url: {}", self.url);
+
         let analytics_response = self
             .client
             .get(self.url.as_str())
             .query(&request_params)
             .send()
-            .await
-            .unwrap();
+            .await?;
 
         let parsed_pool_info = match analytics_response.json::<PoolResponse>().await {
             Ok(resp) => {
@@ -87,5 +89,41 @@ impl Analytics {
             launch_type: parsed_pool_info.pool.launch_type,
             created_on: Duration::from_secs(parsed_pool_info.metrics.created_on),
         })
+    }
+}
+
+mod tests {
+    use log::error;
+    use crate::analytics::PoolResponse;
+
+    #[tokio::test]
+    async fn parse_fee_switch_datum_mainnet() {
+        let client = reqwest::Client::new();
+
+        let request_params = vec![(
+            "asset",
+            format!("{}.{}", "2b3bf22efec7742d2a193d5d1547f14d0f5f33e1da3a7eaa2ed13ce9", "41444120475245415420414741494e"),
+        )];
+
+        let analytics_response = client
+            .get("https://api5.splash.trade/platform-api/v2/snekfun/pool/overview/by")
+            .query(&request_params)
+            .send()
+            .await
+            .unwrap();
+
+        let a = match analytics_response.json::<PoolResponse>().await {
+            Ok(resp) => {
+                Some(resp)
+            }
+            Err(err) => {
+                println!("Error {:?} during request: {:?}", err, request_params);
+                None
+            }
+        };
+
+        println!("{:?}", a);
+
+        assert_eq!(1, 2)
     }
 }
