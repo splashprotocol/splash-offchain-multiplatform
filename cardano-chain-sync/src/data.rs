18b8bc63ef129939d76c364a558a5abac043373e
oskin1 <ilya.arcadich@gmail.com>
2024-12-27 14:47:33 +0300

    WIP.

--- cardano-chain-sync/src/data.rs
+++ cardano-chain-sync/src/data.rs
@@ -7,6 +7,18 @@ pub enum LedgerBlockEvent<Block> {
     RollBackward(Block),
 }
 
+impl<Block> LedgerBlockEvent<Block> {
+    pub fn map<T2, F>(self, f: F) -> LedgerBlockEvent<T2>
+    where
+        F: FnOnce(Block) -> T2,
+    {
+        match self {
+            LedgerBlockEvent::RollForward(blk) => LedgerBlockEvent::RollForward(f(blk)),
+            LedgerBlockEvent::RollBackward(blk) => LedgerBlockEvent::RollBackward(f(blk)),
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub enum LedgerTxEvent<Tx> {
     TxApplied {
