97c02d2ebcc925ff399a4cbf637ebba1cffb7a40
Bromel777 <alexbromel@gmail.com>
2025-04-28 21:58:20 +0300

    DEX-743. Fix farm activation event processing — reset active gauges to empty vector if gauge is missing from DB

--- splash-lp-indexer/src/pipeline/resolve_gauges.rs
+++ splash-lp-indexer/src/pipeline/resolve_gauges.rs
@@ -47,26 +47,31 @@ async fn resolve_events<I: VoteEscrowIndex, DB: Accounts>(
                 index.put_gauge(farm_id, pool_id).await;
             }
             StatelessOnChainEvent::PollFactoryUpdated(PollFactoryUpdated { new_state }) => {
-                if let Some(old_state) = index.get_poll_factory_snapshot().await {
-                    let old_gauges: HashSet<FarmId> = HashSet::from_iter(old_state.active_farms);
-                    let new_gauges = HashSet::from_iter(new_state.active_farms.clone());
-                    let removed_gauges = old_gauges.difference(&new_gauges);
-                    for gauge in removed_gauges {
-                        if let Some(pool_id) = index.get_gauge_binding(*gauge).await {
-                            translated_events.push(OnChainEvent::FarmEvent(FarmEvent::FarmDeactivated(
-                                FarmDeactivated { pool_id },
-                            )))
-                        }
-                    }
-                    let added_gauges = new_gauges.difference(&old_gauges);
-                    for gauge in added_gauges {
-                        if let Some(pool_id) = index.get_gauge_binding(*gauge).await {
-                            translated_events.push(OnChainEvent::FarmEvent(FarmEvent::FarmActivated(
-                                FarmActivated { pool_id },
-                            )))
-                        }
+                let old_state_gauges = index
+                    .get_poll_factory_snapshot()
+                    .await
+                    .map(|old_state| old_state.active_farms)
+                    .unwrap_or(vec![]);
+
+                let old_gauges: HashSet<FarmId> = HashSet::from_iter(old_state_gauges);
+                let new_gauges = HashSet::from_iter(new_state.active_farms.clone());
+                let removed_gauges = old_gauges.difference(&new_gauges);
+                for gauge in removed_gauges {
+                    if let Some(pool_id) = index.get_gauge_binding(*gauge).await {
+                        translated_events.push(OnChainEvent::FarmEvent(FarmEvent::FarmDeactivated(
+                            FarmDeactivated { pool_id },
+                        )))
                     }
                 }
+                let added_gauges = new_gauges.difference(&old_gauges);
+                for gauge in added_gauges {
+                    if let Some(pool_id) = index.get_gauge_binding(*gauge).await {
+                        translated_events.push(OnChainEvent::FarmEvent(FarmEvent::FarmActivated(
+                            FarmActivated { pool_id },
+                        )))
+                    }
+                }
+
                 index.update_poll_factory_snapshot(new_state).await;
             }
             StatelessOnChainEvent::Position(e) => {
