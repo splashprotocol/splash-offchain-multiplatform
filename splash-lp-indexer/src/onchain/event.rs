238e5ce033e48e88d0e0d74db41f1c86d9036f60
Bromel777 <alexbromel@gmail.com>
2025-05-01 12:07:55 +0300

    DEX-746. Fix parsing of smart farm creation in case of empty pool. Fix pool frame processing in case when deposits/redeems are not presented in pool frame

--- splash-lp-indexer/src/onchain/event.rs
+++ splash-lp-indexer/src/onchain/event.rs
@@ -1,4 +1,5 @@
 use crate::config::HarvestLimits;
+use crate::onchain::event::PollFactoryEvents::{FactoryStateUpdate, NewFactory};
 use crate::tx_view::TxViewPartiallyResolved;
 use cml_chain::address::Address;
 use cml_chain::certs::Credential;
@@ -20,7 +21,6 @@ use splash_dao_offchain::entities::onchain::smart_farm::{FarmId, SmartFarmSnapsh
 use splash_dao_offchain::protocol_config::{FarmAuthPolicy, PermManagerAuthPolicy, WPFactoryAuthPolicy};
 use splash_dao_offchain::routines::{ProvideTimedOref, Slot, TimedOutputRef};
 use std::collections::HashSet;
-use crate::onchain::event::PollFactoryEvents::{FactoryStateUpdate, NewFactory};
 
 /// Events extracted from on-chain transactions.
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
@@ -62,9 +62,7 @@ where
         PositionEvent::try_from_ledger(repr, ctx)
             .map(StatelessOnChainEvent::Position)
             .or_else(|| FarmCreated::try_from_ledger(repr, ctx).map(StatelessOnChainEvent::FarmCreated))
-            .or_else(|| {
-                PollFactoryEvents::try_from_ledger(repr, ctx).map(StatelessOnChainEvent::PollFactory)
-            })
+            .or_else(|| PollFactoryEvents::try_from_ledger(repr, ctx).map(StatelessOnChainEvent::PollFactory))
             .or_else(|| {
                 MultipleAccountsHarvest::try_from_ledger(repr, ctx)
                     .map(StatelessOnChainEvent::MultipleHarvest)
@@ -146,7 +144,7 @@ where
         if let Some(pool) = PoolDiff::try_from_ledger(repr, ctx) {
             let (plus_sign, diff) = pool.lp_diff;
             if diff != 0 {
-                if let Some(account) = find_lp_recv(pool.lp_asset.into_token().unwrap(), repr) {
+                if let Some(account) = find_lp_recv(pool.lp_asset.into_token().unwrap(), pool.pool_id, repr) {
                     let account = account.payment_cred().unwrap().clone();
                     return Some(if plus_sign {
                         PositionEvent::Deposit(Deposit {
@@ -230,13 +228,19 @@ pub struct Deposit {
     pub lp_supply: u64,
 }
 
-fn find_lp_recv(Token(pol, tn): Token, tx: &TxViewPartiallyResolved) -> Option<Address> {
+fn find_lp_recv(Token(pol, tn): Token, PoolId(Token(pool_nft_pol, pool_nft_tn)): PoolId, tx: &TxViewPartiallyResolved) -> Option<Address> {
     tx.outputs.iter().find_map(|output| {
-        output
-            .value()
-            .multiasset
-            .get(&pol, &tn.into())
-            .map(|_| output.address().clone())
+        if output.value().multiasset.get(&pol, &tn.into()).is_some()
+            && output
+                .value()
+                .multiasset
+                .get(&pool_nft_pol, &pool_nft_tn.into())
+                .is_none()
+        {
+            Some(output.address().clone())
+        } else {
+            None
+        }
     })
 }
 
@@ -332,7 +336,7 @@ where
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 pub enum PollFactoryEvents {
     NewFactory(PollFactory),
-    FactoryStateUpdate(PollFactoryUpdated)
+    FactoryStateUpdate(PollFactoryUpdated),
 }
 
 impl<Cx> TryFromLedger<TxViewPartiallyResolved, Cx> for PollFactoryEvents
@@ -357,11 +361,9 @@ where
                 let oref = TimedOutputRef::new(OutputRef::new(repr.hash, ix as u64), Slot(0));
                 PollFactorySnapshot::try_from_ledger(utxo, &ProvideTimedOref(ctx, oref)).map(|snapshot| {
                     if factory_in_inputs.contains(&snapshot.get().stable_id) {
-                        FactoryStateUpdate(
-                            PollFactoryUpdated {
-                                new_state: snapshot.get().clone(),
-                            }
-                        )
+                        FactoryStateUpdate(PollFactoryUpdated {
+                            new_state: snapshot.get().clone(),
+                        })
                     } else {
                         NewFactory(snapshot.get().clone())
                     }
---
+++ splash-lp-indexer/src/position_db/pool_frames.rs
@@ -0,0 +1,35 @@
+use async_trait::async_trait;
+use crate::position_db::{PositionDB, ACCOUNTS_CF, POOL_LQ_FRAMES_INDEX_CF};
+use rocksdb::ReadOptions;
+use spectrum_offchain_cardano::data::PoolId;
+use tokio::task::spawn_blocking;
+
+#[async_trait]
+pub trait PoolFrames {
+    async fn get_pool_lq_supply(&self, pool_id: PoolId) -> Option<u64>;
+}
+
+#[async_trait]
+impl PoolFrames for PositionDB {
+    async fn get_pool_lq_supply(&self, pool_id: PoolId) -> Option<u64> {
+        let db = self.db.clone();
+        spawn_blocking(move || {
+            let tx = db.transaction();
+            let pool_lq_frames_cf = db.cf_handle(POOL_LQ_FRAMES_INDEX_CF).unwrap();
+            let readopts = ReadOptions::default();
+            let lq_supply = if let Ok(Some(raw_lq_value)) = tx.get_cf_opt(
+                pool_lq_frames_cf,
+                &rmp_serde::to_vec(&pool_id).unwrap(),
+                &readopts,
+            ) {
+                rmp_serde::from_slice::<u64>(&raw_lq_value).ok()
+            } else {
+                None
+            };
+            tx.commit().unwrap();
+            lq_supply
+        })
+        .await
+        .unwrap()
+    }
+}
