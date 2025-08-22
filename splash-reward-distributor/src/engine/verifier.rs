89ab0dba9c24c28a4ca7f7a36385df69bd2bbb3c
Tim Ling <791016+kettlebell@users.noreply.github.com>
2025-08-22 20:27:19 +1000

    Impl Withdrawal parsing for Verifier (#225)

--- splash-reward-distributor/src/engine/verifier.rs
+++ splash-reward-distributor/src/engine/verifier.rs
@@ -4,11 +4,17 @@ use crate::engine::withdrawal::Withdrawal;
 use crate::positions::{AccountState, Positions};
 use cml_chain::certs::Credential;
 use cml_chain::transaction::Transaction;
+use cml_crypto::Ed25519KeyHash;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use spectrum_cardano_lib::OutputRef;
+use spectrum_cardano_lib::{NetworkId, OutputRef};
+use spectrum_offchain::domain::Has;
 use spectrum_offchain::ledger::TryFromLedger;
 use spectrum_offchain::tx_prover::TxProver;
+use spectrum_offchain_cardano::deployment::DeployedScriptInfo;
+use splash_dao_offchain::deployment::ProtocolValidator;
+use splash_dao_offchain::protocol_config::{BufferWalletScript, SplashPolicy};
+use splash_yf_offchain::settings::MinLovelacePerHarvest;
 use std::marker::PhantomData;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -68,8 +74,8 @@ impl RemoteVerifier<PartiallySignedCardanoTx, Transaction> for HttpVerifier {
 }
 
 #[async_trait::async_trait]
-pub trait LocalVerifier<PartialTx, Tx> {
-    async fn try_approve(&self, tx: &PartialTx) -> Option<Tx>;
+pub trait LocalVerifier<PartialTx, Tx, Ctx> {
+    async fn try_approve(&self, tx: &PartialTx, ctx: &Ctx) -> Option<Tx>;
 }
 
 pub struct Verifier<Tx, Index, Emission, Prover> {
@@ -79,33 +85,48 @@ pub struct Verifier<Tx, Index, Emission, Prover> {
     pd: PhantomData<Tx>,
 }
 
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+pub struct AuthorizedExecutors(pub Vec<Ed25519KeyHash>);
+
 #[async_trait::async_trait]
-impl<Index, Emiss, Prov> LocalVerifier<PartiallySignedCardanoTx, Transaction>
+impl<Index, Emiss, Prov, Ctx> LocalVerifier<PartiallySignedCardanoTx, Transaction, Ctx>
     for Verifier<PartiallySignedCardanoTx, Index, Emiss, Prov>
 where
     Index: Positions<OutputRef> + Send + Sync,
     Emiss: Emission + Send + Sync,
     Prov: TxProver<PartiallySignedCardanoTx, Transaction> + Send + Sync,
+    Ctx: Has<MinLovelacePerHarvest>
+        + Has<DeployedScriptInfo<{ ProtocolValidator::HarvestOrder as u8 }>>
+        + Has<NetworkId>
+        + Has<SplashPolicy>
+        + Has<AuthorizedExecutors>
+        + Has<BufferWalletScript>
+        + Sync,
 {
-    async fn try_approve(&self, tx: &PartiallySignedCardanoTx) -> Option<Transaction> {
-        if let Some(withdrawal) = Withdrawal::<OutputRef>::try_from_ledger(tx, &()) {
-            let order = withdrawal.order;
-            if let Ok(AccountState {
-                total_share_bps,
-                activated_at,
-            }) = self
-                .index
-                .query_account(&Credential::new_pub_key(order.account_key))
-                .await
-            {
-                let emission = self
-                    .emission
-                    .total_emission_between(activated_at, order.issued_at.0);
-                let payout = reward_amount(total_share_bps, emission);
-                if payout == withdrawal.amount {
-                    return Some(self.prover.prove(tx.clone()));
+    async fn try_approve(&self, tx: &PartiallySignedCardanoTx, ctx: &Ctx) -> Option<Transaction> {
+        if let Some(withdrawals) = <Vec<Withdrawal<OutputRef>>>::try_from_ledger(tx, ctx) {
+            for withdrawal in withdrawals {
+                let order = withdrawal.order;
+                if let Ok(AccountState {
+                    total_share_bps,
+                    activated_at,
+                }) = self
+                    .index
+                    .query_account(&Credential::new_pub_key(order.account_key))
+                    .await
+                {
+                    let emission = self
+                        .emission
+                        .total_emission_between(activated_at, order.issued_at.0);
+                    let payout = reward_amount(total_share_bps, emission);
+                    if payout != withdrawal.amount {
+                        return None;
+                    }
+                } else {
+                    return None;
                 }
             }
+            return Some(self.prover.prove(tx.clone()));
         }
         None
     }
--- splash-reward-distributor/src/engine/withdrawal.rs
+++ splash-reward-distributor/src/engine/withdrawal.rs
@@ -1,6 +1,25 @@
-use crate::engine::resolved_tx::PartiallySignedCardanoTx;
-use spectrum_offchain::ledger::TryFromLedger;
-use splash_yf_offchain::entities::harvest_order::HarvestOrder;
+use crate::engine::{
+    resolved_tx::{CardanoTxInput, PartiallySignedCardanoTx},
+    verifier::AuthorizedExecutors,
+};
+use spectrum_cardano_lib::{
+    transaction::TransactionOutputExtension, value::ValueExtension, AssetClass, AssetName, NetworkId,
+    OutputRef, Token,
+};
+use spectrum_offchain::{domain::Has, ledger::TryFromLedger};
+use spectrum_offchain_cardano::deployment::DeployedScriptInfo;
+use splash_dao_offchain::{
+    constants::SPLASH_NAME,
+    deployment::ProtocolValidator,
+    protocol_config::{BufferWalletScript, SplashPolicy},
+};
+use splash_yf_offchain::{
+    entities::{
+        buffer_wallet::try_extract_buffer_wallet,
+        harvest_order::{try_extract_harvest_order, HarvestOrder},
+    },
+    settings::MinLovelacePerHarvest,
+};
 
 #[derive(Debug)]
 pub struct Withdrawal<OrderId> {
@@ -10,8 +29,77 @@ pub struct Withdrawal<OrderId> {
     pub amount: u64,
 }
 
-impl<OrderId, Ctx> TryFromLedger<PartiallySignedCardanoTx, Ctx> for Withdrawal<OrderId> {
+impl<Ctx> TryFromLedger<PartiallySignedCardanoTx, Ctx> for Vec<Withdrawal<OutputRef>>
+where
+    Ctx: Has<MinLovelacePerHarvest>
+        + Has<DeployedScriptInfo<{ ProtocolValidator::HarvestOrder as u8 }>>
+        + Has<NetworkId>
+        + Has<AuthorizedExecutors>
+        + Has<BufferWalletScript>
+        + Has<SplashPolicy>,
+{
     fn try_from_ledger(repr: &PartiallySignedCardanoTx, ctx: &Ctx) -> Option<Self> {
-        todo!("DEX-906")
+        let splash_asset_name = AssetName::from_utf8(SPLASH_NAME.into());
+        let splash_policy = ctx.select::<SplashPolicy>().0;
+        let splash_asset_class = AssetClass::Token(Token(splash_policy, splash_asset_name));
+        let network_id = ctx.select::<NetworkId>();
+
+        let mut buffer_wallet_spent = false;
+
+        let withdrawals: Vec<_> = repr
+            .inputs
+            .iter()
+            .filter_map(
+                |CardanoTxInput {
+                     output_ref,
+                     tx_output,
+                     issued_at,
+                 }| {
+                    let res = issued_at.and_then(|issued_at| {
+                        try_extract_harvest_order(tx_output, *output_ref, issued_at, ctx).and_then(|order| {
+                            repr.tx.body.outputs.iter().find_map(|tx_output| {
+                                if *tx_output.address() == order.reward_receiver.to_address(network_id) {
+                                    let amount = tx_output.value().amount_of(splash_asset_class)?;
+                                    return Some(Withdrawal {
+                                        order: order.clone(),
+                                        amount,
+                                    });
+                                }
+                                None
+                            })
+                        })
+                    });
+
+                    if res.is_none() && !buffer_wallet_spent {
+                        buffer_wallet_spent =
+                            try_extract_buffer_wallet(tx_output, *output_ref, ctx).is_some();
+                    }
+
+                    res
+                },
+            )
+            .collect();
+
+        let valid_tx_signature = repr
+            .tx
+            .body
+            .required_signers
+            .as_ref()
+            .map(|signers| {
+                if signers.len() == 1 {
+                    let signer_key_hash = signers.first().unwrap();
+                    let authorized_signers = ctx.select::<AuthorizedExecutors>().0;
+                    authorized_signers.contains(signer_key_hash)
+                } else {
+                    false
+                }
+            })
+            .unwrap_or(false);
+
+        if buffer_wallet_spent && !withdrawals.is_empty() && valid_tx_signature {
+            Some(withdrawals)
+        } else {
+            None
+        }
     }
 }
